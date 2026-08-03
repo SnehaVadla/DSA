7a21ddcd1f439fd6948c5ed9783fce68e0ff9e78
Sneha Vadla <vadlasneha2005@gmail.com>
2026-08-03 22:08:42 +0530

    Add Games [A] from Codeforces

---
+++ 268/A - Games/solution.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int n = sc.nextInt();
+        int[] h = new int[n];
+        int[] g = new int[n];
+ 
+        for (int i = 0; i < n; i++) {
+            h[i] = sc.nextInt();
+            g[i] = sc.nextInt();
+        }
+ 
+        int count = 0;
+ 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i != j && h[i] == g[j]) {
+                    count++;
+                }
+            }
+        }
+ 
+        System.out.println(count);
+    }
+}
