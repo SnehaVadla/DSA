f8a3b172179c64bd882103709461540309daee75
Sneha Vadla <vadlasneha2005@gmail.com>
2026-08-03 22:09:17 +0530

    Add Even Array [B] from Codeforces

---
+++ 1367/B - Even Array/solution.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+ 
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] a = new int[n];
+ 
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+ 
+            int e = 0; 
+            int o = 0;
+ 
+            for (int i = 0; i < n; i++) {
+                if (i % 2 != a[i] % 2) {
+                    if (i % 2 == 0) {
+                        e++;
+                    } else {
+                        o++;
+                    }
+                }
+            }
+ 
+            if (e == o) {
+                System.out.println(e);
+            } else {
+                System.out.println(-1);
+            }
+        }
+    }
+}
