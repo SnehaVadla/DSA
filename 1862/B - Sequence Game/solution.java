bfe23864227872eadc24d924dba5a391d5952d36
Sneha Vadla <vadlasneha2005@gmail.com>
2026-08-09 21:33:58 +0530

    Add Sequence Game [B] from Codeforces

---
+++ 1862/B - Sequence Game/solution.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+ 
+        int t = sc.nextInt();
+ 
+        while (t-- > 0) {
+            int n = sc.nextInt();
+ 
+            int[] b = new int[n];
+ 
+            for (int i = 0; i < n; i++) {
+                b[i] = sc.nextInt();
+            }
+ 
+            ArrayList<Integer> a = new ArrayList<>();
+ 
+            a.add(b[0]);
+ 
+            for (int i = 1; i < n; i++) {
+ 
+                if (b[i - 1] > b[i]) {
+                    a.add(1);
+                }
+ 
+                a.add(b[i]);
+            }
+ 
+            System.out.println(a.size());
+ 
+            for (int x : a) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+    }
+}
