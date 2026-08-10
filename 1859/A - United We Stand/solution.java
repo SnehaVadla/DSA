344c3e02a42085f53308ba22eab2203af14fbef5
Sneha Vadla <vadlasneha2005@gmail.com>
2026-08-10 22:51:42 +0530

    Add United We Stand [A] from Codeforces

---
+++ 1859/A - United We Stand/solution.java
@@ -0,0 +1,41 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] a = new int[n];
+            int min = Integer.MAX_VALUE;
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+                min = Math.min(min, a[i]);
+            }
+            ArrayList<Integer> b = new ArrayList<>();
+            ArrayList<Integer> c = new ArrayList<>();
+            for (int x : a) {
+                if (x == min) {
+                    b.add(x);
+                } else {
+                    c.add(x);
+                }
+            }
+            if (c.isEmpty()) {
+                System.out.println(-1);
+                continue;
+            }
+            System.out.println(b.size() + " " + c.size());
+ 
+            for (int x : b) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+ 
+            for (int x : c) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+    }
+}
