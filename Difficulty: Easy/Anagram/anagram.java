8562cd41659e1c5ec4997eec920797ff667506f0
Sneha Vadla <vadlasneha2005@gmail.com>
2026-08-11 23:23:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Anagram/anagram.java
@@ -0,0 +1,25 @@
+class Solution {
+    public static boolean areAnagrams(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        int[] freq = new int[26];
+
+        for (char ch : s1.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        for (char ch : s2.toCharArray()) {
+            freq[ch - 'a']--;
+        }
+
+        for (int count : freq) {
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
