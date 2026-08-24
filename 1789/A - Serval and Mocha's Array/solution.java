8caeb50b6295fff019e93282826e3a326e383dbe
Sneha Vadla <vadlasneha2005@gmail.com>
2026-08-24 23:27:58 +0530

    Add Serval and Mocha's Array [A] from Codeforces

---
+++ 1789/A - Serval and Mocha's Array/solution.java
@@ -0,0 +1,48 @@
+import java.util.*;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int t = sc.nextInt();
+ 
+        while (t-- > 0) {
+            int n = sc.nextInt();
+ 
+            int[] a = new int[n];
+ 
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+ 
+            boolean possible = false;
+ 
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) {
+ 
+                    if (gcd(a[i], a[j]) <= 2) {
+                        possible = true;
+                        break;
+                    }
+                }
+ 
+                if (possible) {
+                    break;
+                }
+            }
+ 
+            System.out.println(possible ? "Yes" : "No");
+        }
+    }
+ 
+    static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+ 
+        return a;
+    }
+}
